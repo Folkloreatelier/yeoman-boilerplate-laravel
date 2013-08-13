917278c7d7a81c5e0cd1560dbc6f82631168a0cd
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2013-08-13 11:22:40 -0400

    Added auto hashing of password

--- app/models/User.php
+++ app/models/User.php
@@ -5,19 +5,24 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
+
 	protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
+
+	protected $visible = array('email');
 	protected $hidden = array('password');
+	protected $fillable = array('email','password');
+
+
+	/*
+	 *
+	 * Relationships
+	 *
+	 */
+	public function setPasswordAttribute($value) {
+    	$this->attributes['password'] = Hash::make($value);
+	}
+
 
 	/**
 	 * Get the unique identifier for the user.
