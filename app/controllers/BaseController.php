5b07d597b80b366f4575f036d819ee5720f513fb
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2014-02-26 12:04:39 -0500

    Added language property

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -3,6 +3,12 @@
 class BaseController extends Controller {
 
 	protected $layout = 'layouts.main';
+	protected $language;
+
+	public function __construct()
+	{
+		$this->language = Config::get('app.locale');
+	}
 
 	/**
 	 * Setup the layout used by the controller.
